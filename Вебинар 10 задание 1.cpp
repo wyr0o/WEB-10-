49e4b64fc149ab88e58f035cb33e8b2288208315
wyr0o <brykin-2006@mail.ru>
2025-04-10 17:10:46 +0300

    Add files via upload

---
+++ Вебинар 10 задание 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void inputArr(int size, int* arr)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "arr[" << i << "] = ";
+		std::cin >> arr[i];
+
+	}
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	std::cout << "введите размер массива: ";
+	int size = 0;
+	std::cin >> size;
+
+	int* arr = new int[size]();
+
+	inputArr(size, arr);
+	std::cout << "Введённый массив: ";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i]<< " ";
+	}
+	delete[] arr;
+}
