d31702742c9f62b0081e3a4c72ad743c94f41fdb
wyr0o <brykin-2006@mail.ru>
2025-04-10 17:24:08 +0300

    Add files via upload

---
+++ вебинар 10 задание 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+double* create_array(int size)
+{
+	return new double[size]();
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	int size = 0;
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+	double* arr = create_array(size);
+	std::cout << "Массив: ";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+
+	delete[] arr;
+}
