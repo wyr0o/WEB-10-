9e1918c2d3a3b188ad73b56b572c37314499c09f
wyr0o <brykin-2006@mail.ru>
2025-04-10 17:51:57 +0300

    Add files via upload

---
+++ вебинар 10 задание 3.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int** create_two_dim_array(int rows, int columns)
+{
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++) {
+		arr[i] = new int[columns]();  
+	}
+	return arr;
+}
+
+void fill_two_dim_array(int** arr, int rows, int columns) 
+{
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < columns; ++j)
+		{
+			arr[i][j] = (i+1) * (j+1);
+		}
+	}
+}
+
+void print_two_dim_array(int** arr, int rows, int columns)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << arr[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void delete_two_dim_array(int** arr, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int rows = 0;
+	int columns = 0;
+	std::cout << "Введите количество строк: ";
+	std::cin >> rows;
+	std::cout << "Введите количество столбцов: ";
+	std::cin >> columns;
+	int** arr =create_two_dim_array(rows, columns);
+	fill_two_dim_array(arr, rows, columns);
+	print_two_dim_array(arr, rows, columns);
+	delete_two_dim_array(arr, rows);
+
+}
